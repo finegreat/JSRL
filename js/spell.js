f28be70dac151ca7e98a7b34b3d304885854ed87
Lewis <93319168+finegreat@users.noreply.github.com>
2023-06-19 17:36:59 +0100

    Add files via upload

---
+++ js/spell.js
@@ -0,0 +1,3 @@
+//TODO: implementation of the pickups to be included on the floor
+//maybe in the form of scrolls?? resulting in a single use pickup that will enable upon keypress
+//variation of abilities possible: fire (instant dmg), ice (freeze for # of turns, maybe DoT), water (waterlog tiles, resulting in 2 movements to get off the tile)
